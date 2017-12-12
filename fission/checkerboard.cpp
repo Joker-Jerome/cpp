9297b7b9e0768e3c6a85a40ffce79d5349b9c1b7
Jerome Yu <jerome1993@qq.com>
2017-12-11 20:56:59 -0500

    fission

---
+++ fission/checkerboard.cpp
@@ -0,0 +1,305 @@
+#include <vector>
+#include <memory>
+#include <iostream>
+#include <sstream>
+
+
+#include "checkerboard.h"
+#include "checker.h"
+#include "lchecker.h"
+
+#include "math.h"
+
+namespace cs427_527
+{
+  CheckerBoard::CheckerBoard()
+    : board(8, std::vector<std::shared_ptr<Piece>>(8, nullptr)),
+      width(8),
+      height(8),
+      turn(0),
+      jumping(nullptr)
+  {
+    for (int r = 0; r < height; r++)
+      {
+    	for (int c = r % 2; c < width; c += 2)
+	  {
+	    board[r][c] = std::make_shared<Checker>(0, r, c);
+	  }
+      }
+
+    for (int r = 0; r < height; r++)
+      {
+	for (int c = 1-(r % 2); c < width; c += 2)
+	  {
+	    board[r][c] = std::make_shared<Checker>(1, r, c);
+
+	  }
+      }
+    for (int r = 0; r < height; r++) {
+    		board[r][0] = nullptr;
+		board[r][width-1] = nullptr;
+    }
+    for (int c = 1; c < width-1; c++) {
+        	board[0][c] = nullptr;
+    		board[height-1][c] = nullptr;
+    }
+    for (int r = height/2 - 2, i = 1 ; r > 0; r--, i++){
+		for (int j = 0; j < i; j++) {
+			board[r][1 + j] = nullptr;
+			board[r][width - 2 - j] = nullptr;
+		}
+
+    }
+    for (int r = height/2 + 1, i = 1 ; r < height - 1; r++, i++){
+    		for (int j = 0; j < i; j++) {
+			board[r][1 + j] = nullptr;
+			board[r][width - 2 - j] = nullptr;
+		}
+    }
+    for (int i = 0; i < width; i++) {
+    		int count = 0;
+    		for (int j = 0; j < height; j++) {
+    			if (board[i][j] != nullptr) {
+    				count++;
+    			}
+    		}
+    		if (count == 4) {
+    			for(int h = height/2 - 2; h < height/2 + 2; h++) {
+    				//Piece tmp_piece = board[i][h];
+    				int tmp_player = board[h][i]->getPlayer();
+    				board[h][i] = std::make_shared<LChecker>(tmp_player, h, i);
+    			}
+    		}
+    }
+
+  }
+
+  CheckerBoard::CheckerBoard(int row,int col)
+      : board(row, std::vector<std::shared_ptr<Piece>>(col, nullptr)),
+        width(col),
+        height(row),
+        turn(0),
+        jumping(nullptr)
+    {
+      for (int r = 0; r < height; r++)
+        {
+      	for (int c = r % 2; c < width; c += 2)
+  	  {
+  	    board[r][c] = std::make_shared<Checker>(0, r, c);
+  	  }
+        }
+
+      for (int r = 0; r < height; r++)
+        {
+  	for (int c = 1-(r % 2); c < width; c += 2)
+  	  {
+  	    board[r][c] = std::make_shared<Checker>(1, r, c);
+
+  	  }
+        }
+      for (int r = 0; r < height; r++) {
+      		board[r][0] = nullptr;
+  		board[r][width-1] = nullptr;
+      }
+      for (int c = 1; c < width-1; c++) {
+          	board[0][c] = nullptr;
+      		board[height-1][c] = nullptr;
+      }
+
+      for (int r = height/2 - 2, i = 1 ; r > 0; r--, i++){
+  		for (int j = 0; j < i; j++) {
+  			board[r][1 + j] = nullptr;
+  			board[r][width - 2 - j] = nullptr;
+  		}
+
+      }
+
+      for (int r = height/2 + 1, i = 1 ; r < height - 1; r++, i++){
+      		for (int j = 0; j < i; j++) {
+  			board[r][1 + j] = nullptr;
+  			board[r][width - 2 - j] = nullptr;
+  		}
+      }
+
+      for (int i = 0; i < width; i++) {
+      		int count = 0;
+      		for (int j = 0; j < height; j++) {
+      			if (board[j][i] != nullptr) {
+      				count++;
+      			}
+      		}
+
+
+      		if (count == 4) {
+      			for(int h = height/2 - 2; h < height/2 + 2; h++) {
+      				//Piece tmp_piece = board[i][h];
+      				int tmp_player = board[h][i]->getPlayer();
+      				board[h][i] = std::make_shared<LChecker>(tmp_player, h, i);
+      			}
+      		}
+      }
+
+    }
+
+
+  CheckerBoard::~CheckerBoard()
+  {
+  }
+  
+  int CheckerBoard::getWidth() const
+  {
+    return width;
+  }
+
+  int CheckerBoard::getHeight() const
+  {
+    return height;
+  }
+
+  std::shared_ptr<Piece> CheckerBoard::getPiece(int r, int c)
+  {
+    if (inBounds(r, c))
+      {
+	return board[r][c];
+      }
+    else
+      {
+	return nullptr;
+      }
+  }
+
+  std::shared_ptr<const Piece> CheckerBoard::getPiece(int r, int c) const
+  {
+    if (inBounds(r, c))
+      {
+	return board[r][c];
+      }
+    else
+      {
+	return nullptr;
+      }
+  }
+
+  void CheckerBoard::removePiece(int r, int c)
+  {
+    if (inBounds(r, c))
+      {
+	board[r][c] = nullptr;
+      }
+  }
+
+  void CheckerBoard::placePiece(int r, int c, std::shared_ptr<Piece> p)
+  {
+    if (inBounds(r, c))
+      {
+	board[r][c] = p;
+      }
+  }
+
+  PieceBoard::Color CheckerBoard::getBoardColor(int r, int c) const
+  {
+    if (r % 2 == c % 2)
+      {
+	return PieceBoard::Color::RED;
+      }
+    else
+      {
+	return PieceBoard::Color::BLACK;
+      }
+  }
+
+  bool CheckerBoard::isLegalMove(int fromR, int fromC, int toR, int toC) const
+  {
+    if (!inBounds(fromR, fromC) || !inBounds(toR, toC))
+      {
+    		return false;
+      }
+
+	// move is legal if is moves current player's checker legally
+	std::shared_ptr<const Piece> moving = getPiece(fromR, fromC);
+	return (moving != nullptr
+		&& moving->getPlayer() == turn
+		&& moving->isLegalMove(*this, toR, toC));
+
+  }
+
+
+
+  void CheckerBoard::makeMove(int fromR, int fromC, int toR, int toC)
+  {
+    if (isLegalMove(fromR, fromC, toR, toC))
+      {
+    		std::shared_ptr<Piece> moving = getPiece(fromR, fromC);
+    		moving->makeMove(*this, toR, toC);
+    		this->turn = 1 - turn;
+      }
+  }
+
+  int CheckerBoard::getCurrentPlayer() const
+  {
+    return turn;
+  }
+
+  bool CheckerBoard::gameOver() const
+  {
+    // game is over if no moves for current player
+	int countCurrent = 0;
+	int countOpponent = 0;
+	int currentPlayer = getCurrentPlayer();
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if (getPiece(i,j)->player == currentPlayer) {
+				countCurrent++;
+			} else (getPiece(i,j)->player == (1-currentPlayer)) {
+				countOpponent++;
+			}
+
+		}
+	}
+	if (countCurrent == 0 && countOpponent > 0) {
+		std::cout << "Player" << currentPlayer << " won" << std::endl;
+		return true;
+	} else if (countCurrent > 0 && countOpponent ==  0){
+		std::cout << "Player" << std::string(1-currentPlayer) << " won" << std::endl;
+		return true;
+	}
+    return false;
+  }
+
+  std::string CheckerBoard::toString() const
+  {
+    std::ostringstream os;
+
+    for (int r = 0; r < height; r++)
+      {
+	for (int c = 0; c < width; c++)
+	  {
+	    if (getPiece(r, c) == nullptr)
+	      {
+		if (getBoardColor(r, c) == PieceBoard::Color::RED)
+		  {
+		    os << "..";
+		  }
+		else
+		  {
+		    os << "  ";
+		  }
+	      }
+	    else
+	      {
+		os << getPiece(r, c)->toString();
+	      }
+	  }
+	os << std::endl;
+      }
+
+    return os.str();
+  }
+
+  bool CheckerBoard::inBounds(int r, int c) const
+  {
+    return r >= 0 && r < height && c >= 0 && c < width;
+  }
+  
+}
+
---
+++ fission/checkerboard.h
@@ -0,0 +1,201 @@
+#ifndef __CHECKERBOARD_H__
+#define __CHECKERBOARD_H__
+
+
+#include "pieceboard.h"
+
+namespace cs427_527
+{
+  class Checker;
+
+  class CheckerBoard : public PieceBoard
+  {
+  public:
+    //enum Color { BLACK, RED };
+
+    /**
+     * Creates a checkerboard with pieces in their initial locations.
+     */
+    CheckerBoard();
+
+    /**
+         * Creates a checkerboard with pieces in their initial locations.
+         */
+    CheckerBoard(int row,int col);
+
+    /**
+     * Destoys this board.
+     */
+    virtual ~CheckerBoard();
+    
+    /**
+     * Returns the piece at the given position on this board, or
+     * nullptr if there is no such piece.
+     *
+     * @param row a row on this board
+     * @param col a column on this board
+     */
+    virtual std::shared_ptr<Piece> getPiece(int row, int col);
+
+    /**
+     * Returns the piece at the given position on this board, or
+     * nullptr if there is no such piece.
+     *
+     * @param row a row on this board
+     * @param col a column on this board
+     */
+    virtual std::shared_ptr<const Piece> getPiece(int row, int col) const;
+
+    /**
+     * Removes the piece, if any, at the given position on this board.
+     *
+     * @param row a row on this board
+     * @param col a column on this board
+     */
+    virtual void removePiece(int row, int col);
+
+    /**
+     * Places the given checker at the given location on this board.
+     *
+     * @param row a row on this board
+     * @param col a column on this board
+     * @param p a checker
+     */
+    virtual void placePiece(int row, int col, std::shared_ptr<Piece> p);
+
+    /**
+     * Returns the width of this board.
+     *
+     * @return the width of this board
+     */
+    virtual int getWidth() const;
+
+    /**
+     * Returns the height of this board.
+     *
+     * @return the height of this board
+     */
+    virtual int getHeight() const;
+
+    /**
+     * Returns the color of this board at the given position.
+     *
+     * @param row a row on this board
+     * @param col a column on this board
+     * @return the color of that position.
+     */
+    virtual PieceBoard::Color getBoardColor(int row, int col) const;
+
+    /**
+     * Returns the index of the player whose turn it is on this board.
+     *
+     * @return the player whose turn it is
+     */
+    virtual int getCurrentPlayer() const;
+
+    /**
+     * Determines if it is legal to move a piece from the given
+     * location to the given location on this board.
+     *
+     * @param fromR a row on this board
+     * @param fromC a column on this board
+     * @param toR a row on this board
+     * @param toC a column on this board
+     * @return true if and only if the move is legal
+     */
+    virtual bool isLegalMove(int fromR, int fromC, int toR, int toC) const;
+
+    /**
+     * Updates this move to reflect the effects of the given move.
+     *
+     * @param fromR the starting row of a legal move on this board
+     * @param fromC the starting column of a legal move on this board
+     * @param toR the ending row of a legal move on this board
+     * @param toC the ending column of a legal move on this board
+     */
+    virtual void makeMove(int fromR, int fromC, int toR, int toC);
+
+    /**
+     * Determines if the game on this board is over.
+     *
+     * @return true if and only if the game is over
+     */
+    virtual bool gameOver() const;
+
+    /**
+     * Returns a printable representation of this board.
+     *
+     * @return a printable representation of this board
+     */
+    virtual std::string toString() const;
+    
+    /**
+     * Determines if the given position is on this board.
+     *
+     * @return true if and only if the position is on this board
+     */
+    virtual bool inBounds(int r, int c) const;
+
+
+  protected:
+      /**
+       * Contains the pieces on this board.  board[r][c] is the piece
+       * at row r, column c.  A given entry is nullptr if there is no
+       * piece.
+       */
+      std::vector<std::vector<std::shared_ptr<Piece> > > board;
+
+      /**
+       * The width of this board.
+       */
+      int width;
+
+      /**
+       * The height of this board.
+       */
+      int height;
+
+      /**
+       * The index of the player whose turn it is.
+       */
+      int turn;
+
+      /**
+       * A pointer to the piece that is in the middle of a sequence
+       * of jumps.  This piece is the only one it is legal to move.
+       * If there is no such piece, this is nullptr and it is legal
+       * to move any piece that has a legal move.
+       */
+      std::shared_ptr<Piece> jumping;
+    /**
+     * Returns the distance of the longest legal move on this board.
+     *
+     * @return the distance of the longest legal move
+     */
+    int findLongestMove() const;
+
+    /**
+     * Returns the distance of the longest legal move by the piece
+     * at the given position on this board.
+     *
+     * @param fromR the row of a piece on this board
+     * @param fromC the column of a piece on this board
+     * @return the distance of the longest legal move for the piece
+     * at that position
+     */
+    int findLongestMove(int fromR, int fromC) const;
+
+  };
+
+  /**
+   * Outputs a printable representation of this board to the given stream.
+   *
+   * @param os an output stream
+   * @param board a board
+   * @return the output stream
+   */
+  //std::ostream& operator<<(std::ostream& os, const PieceBoard& board);
+}
+  
+#endif
+
